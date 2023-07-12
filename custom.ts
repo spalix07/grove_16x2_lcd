77a4d10fb5350f9230a2d3580e129f05cba537de
spalix07 <139249782+spalix07@users.noreply.github.com>
2023-07-12 23:53:07 +0200

    Mise à jour custom.ts

--- custom.ts
+++ custom.ts
@@ -7,7 +7,7 @@
 /**
  * Custom blocks
  */
-//% weight=20 color=#0fbc11 icon="▀"
+//% weight=20 color=#AA0000 icon="\uf108"
 namespace I2C_LCD1602 {
     let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574
     let BK: number      // backlight control
@@ -72,29 +72,75 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * initial LCD, set I2C address. Address is 39/63 for PCF8574/PCF8574A
-     * @param Addr is i2c address for LCD, eg: 0, 39, 63. 0 is auto find address
+     * initial LCD, set I2C address. Address is 62 (0x3e)
+     * @param Addr is i2c address for LCD, eg: 62. 
      */
     //% blockId="I2C_LCD1620_SET_ADDRESS" block="LCD initialize with Address %addr"
     //% weight=100 blockGap=8
-    //% parts=LCD1602_I2C trackArgs=0
+    //% addr.defl=0x3e
     export function LcdInit(Addr: number) {
-        if (Addr == 0) i2cAddr = AutoAddr()
-        else i2cAddr = Addr
+        i2cAddr = Addr
         BK = 8
         RS = 0
-        cmd(0x33)       // set 4bit mode
-        basic.pause(5)
-        set(0x30)
-        basic.pause(5)
-        set(0x20)
-        basic.pause(5)
-        cmd(0x28)       // set mode
-        cmd(0x0C)
-        cmd(0x06)
-        cmd(0x01)       // clear
+
+        basic.pause(20) // Attente > 15ms
+        // 8bits mode, 2Lines mode, 5x8 dots
+        pins.i2cWriteNumber(i2cAddr, 0x80, NumberFormat.Int8LE)
+        pins.i2cWriteNumber(i2cAddr, 0x20 | 0x10 | 0x08 | 0x00, NumberFormat.Int8LE)
+        basic.pause(1) // Attente > 39us
+        // Display ON, Cursor ON, Blink OFF
+        pins.i2cWriteNumber(i2cAddr, 0x80, NumberFormat.Int8LE)
+        pins.i2cWriteNumber(i2cAddr, 0x08 | 0x04 | 0x02 | 0x00, NumberFormat.Int8LE)
+        basic.pause(1) // Attente > 39us
+        // Clear
+        pins.i2cWriteNumber(i2cAddr, 0x80, NumberFormat.Int8LE)
+        pins.i2cWriteNumber(i2cAddr, 0x01, NumberFormat.Int8LE)
+        basic.pause(2) // Attente > 1.53ms
+        // Entry left
+        pins.i2cWriteNumber(i2cAddr, 0x80, NumberFormat.Int8LE)
+        pins.i2cWriteNumber(i2cAddr, 0x04 | 0x02 | 0x00, NumberFormat.Int8LE)
     }
 
+/*
+    sleep(20)  # Attente > 15ms
+    self.fct = 0x10 | 0x08 | 0x00  # 8bits mode, 2Lines mode, 5x8 dots
+    i2c.write(0x3e, bytearray([0x80, 0x20 | self.fct]))
+    sleep(1)  # Attente > 39us
+    self.ctrl = 0x04 | 0x02 | 0x00  # Display ON, Cursor ON, Blink OFF
+    i2c.write(0x3e, bytearray([0x80, 0x08 | self.ctrl]))
+    sleep(1)  # Attente > 39us
+    self.clear()
+    sleep(2)  # Attente > 1.53ms
+    self.mod = 0x02 | 0x00  # Entry left
+    i2c.write(0x3e, bytearray([0x80, 0x04 | self.mod]))
+        # self.display(True)
+*/
+
+/*
+    def write_char(self, c): i2c.write(0x3e, bytearray([0x40, c]))
+
+    def setCursor(self, x, y):
+    x = (x | 0x80) if y == 0 else (x | 0xc0)
+    i2c.write(0x3e, bytearray([0x80, x]))
+*/
+
+    //% blockId="I2C_LCD1620_SET_CURSOR" block="set cursor at |x %x |y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    export function setCursor (x: number, y: number): void {
+        if (y==0)
+            x = x | 0x80
+        else
+            x = x | 0xc0
+        
+        pins.i2cWriteNumber(i2cAddr, 0x80, NumberFormat.Int8LE)
+        pins.i2cWriteNumber(i2cAddr, x, NumberFormat.Int8LE)
+    }
+
+   
+        
+   
     /**
      * show a number in LCD at given position
      * @param n is number will be show, eg: 10, 100, 200
@@ -105,10 +151,9 @@ namespace I2C_LCD1602 {
     //% weight=90 blockGap=8
     //% x.min=0 x.max=15
     //% y.min=0 y.max=1
-    //% parts=LCD1602_I2C trackArgs=0
     export function ShowNumber(n: number, x: number, y: number): void {
         let s = n.toString()
-        ShowString(s, x, y)
+        //ShowString(s, x, y)
     }
 
     /**
@@ -117,23 +162,16 @@ namespace I2C_LCD1602 {
      * @param x is LCD column position, [0 - 15], eg: 0
      * @param y is LCD row position, [0 - 1], eg: 0
      */
-    //% blockId="I2C_LCD1620_SHOW_STRING" block="show string %s|at x %x|y %y"
+    //% blockId="I2C_LCD1620_SHOW_STRING" block="show string %s"
     //% weight=90 blockGap=8
-    //% x.min=0 x.max=15
-    //% y.min=0 y.max=1
-    //% parts=LCD1602_I2C trackArgs=0
-    export function ShowString(s: string, x: number, y: number): void {
-        let a: number
-
-        if (y > 0)
-            a = 0xC0
-        else
-            a = 0x80
-        a += x
-        cmd(a)
-
+    export function ShowString(s: string): void {
+     
         for (let i = 0; i < s.length; i++) {
-            dat(s.charCodeAt(i))
+            // dat(s.charCodeAt(i))
+            // i2c.write(0x3e, bytearray([0x40, c])
+            pins.i2cWriteNumber(i2cAddr, 0x40, NumberFormat.Int8LE)
+            pins.i2cWriteNumber(i2cAddr, s.charCodeAt(i), NumberFormat.Int8LE)
+
         }
     }
 
@@ -142,7 +180,6 @@ namespace I2C_LCD1602 {
      */
     //% blockId="I2C_LCD1620_ON" block="turn on LCD"
     //% weight=81 blockGap=8
-    //% parts=LCD1602_I2C trackArgs=0
     export function on(): void {
         cmd(0x0C)
     }
@@ -152,7 +189,6 @@ namespace I2C_LCD1602 {
      */
     //% blockId="I2C_LCD1620_OFF" block="turn off LCD"
     //% weight=80 blockGap=8
-    //% parts=LCD1602_I2C trackArgs=0
     export function off(): void {
         cmd(0x08)
     }
@@ -162,7 +198,6 @@ namespace I2C_LCD1602 {
      */
     //% blockId="I2C_LCD1620_CLEAR" block="clear LCD"
     //% weight=85 blockGap=8
-    //% parts=LCD1602_I2C trackArgs=0
     export function clear(): void {
         cmd(0x01)
     }
@@ -172,7 +207,6 @@ namespace I2C_LCD1602 {
      */
     //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="turn on backlight"
     //% weight=71 blockGap=8
-    //% parts=LCD1602_I2C trackArgs=0
     export function BacklightOn(): void {
         BK = 8
         cmd(0)
