cdabb1387bf5b8ab5af818ff0031a7c6e08be501
spalix07 <139249782+spalix07@users.noreply.github.com>
2023-08-06 23:22:52 +0200

    Mise à jour pxt.json, grove_LCD1602.ts, test.ts

--- grove_LCD1602.ts
+++ grove_LCD1602.ts
@@ -1,12 +1,12 @@
 // Ajouter votre code ici
 namespace smbus {
-    export function writeByte(addr: number, register: number, value: number): void {
+    export function sm_writeByte(addr: number, register: number, value: number): void {
         let temp = pins.createBuffer(2);
         temp[0] = register;
         temp[1] = value;
         pins.i2cWriteBuffer(addr, temp, false);
     }
-    export function writeBuffer(addr: number, register: number, value: Buffer): void {
+    export function sm_writeBuffer(addr: number, register: number, value: Buffer): void {
         let temp = pins.createBuffer(value.length + 1);
         temp[0] = register;
         for (let x = 0; x < value.length; x++) {
@@ -14,19 +14,19 @@ namespace smbus {
         }
         pins.i2cWriteBuffer(addr, temp, false);
     }
-    export function readBuffer(addr: number, register: number, len: number): Buffer {
+    export function sm_readBuffer(addr: number, register: number, len: number): Buffer {
         let temp = pins.createBuffer(1);
         temp[0] = register;
         pins.i2cWriteBuffer(addr, temp, false);
         return pins.i2cReadBuffer(addr, len, false);
     }
-    function readNumber(addr: number, register: number, fmt: NumberFormat = NumberFormat.UInt8LE): number {
+    function sm_readNumber(addr: number, register: number, fmt: NumberFormat = NumberFormat.UInt8LE): number {
         let temp = pins.createBuffer(1);
         temp[0] = register;
         pins.i2cWriteBuffer(addr, temp, false);
         return pins.i2cReadNumber(addr, fmt, false);
     }
-    export function unpack(fmt: string, buf: Buffer): number[] {
+    export function sm_unpack(fmt: string, buf: Buffer): number[] {
         let le: boolean = true;
         let offset: number = 0;
         let result: number[] = [];
@@ -60,72 +60,72 @@ namespace smbus {
 /**
  * Custom blocks
  */
-//% weight=20 color=#FF8800 icon="\uf108"
+//% weight=20 color=#0080FF icon="\uf108"
 namespace GROVE_I2C_LCD1602 {
  
     let i2cAddr: number // 0x3E par défaut
     let regCTRL: number // Registre Control LCD (Display ON/OFF, Cursor ON/OFF, Blink ON/OFF)
 
     /**
-     * initialize LCD, set I2C address : Address is 62 (0x3e)
-     * @param Addr is i2c address for LCD, eg: 62
+     * Initialise l'afficheur LCD (adresse I2C par défaut = 62 (0x3e))
+     * @param Addr : adresse I2C de l'afficheur LCD,  ex: 62
      */
-    //% blockId="GROVE_I2C_LCD1602_SET_ADDRESS" block="[LCD] initialize with Address $Addr"
+    //% blockId="GROVE_I2C_LCD1602_SET_ADDRESS" block="[LCD] Initialiser avec adresse $Addr"
     //% Addr.defl=0x3e
     //% weight=100 blockGap=8
-    export function lcdInit(Addr: number) {
+    export function LCD_init(Addr: number) {
         i2cAddr = Addr
 
         basic.pause(20) // Attente > 15ms
         // 8bits mode, 2Lines mode, 5x8 dots
-        smbus.writeByte(i2cAddr, 0x80, 0x20|0x10|0x08|0x00)
+        smbus.sm_writeByte(i2cAddr, 0x80, 0x20|0x10|0x08|0x00)
         basic.pause(1) // Attente > 39us
 
-        // Display ON, Cursor ON, Blink OFF
-        regCTRL = 0x04|0x02|0x00
-        smbus.writeByte(i2cAddr, 0x80, 0x08|regCTRL)
+        // Display ON, Cursor OFF, Blink OFF
+        regCTRL = 0x04|0x00|0x00
+        smbus.sm_writeByte(i2cAddr, 0x80, 0x08|regCTRL)
         basic.pause(1) // Attente > 39us
 
         // Clear display
         //smbus.writeByte(i2cAddr, 0x80, 0x01)
         //basic.pause(2) // Attente > 1.53ms
-        clearDisplay()
+        LCD_clearDisplay()
 
         // Entry left
-        smbus.writeByte(i2cAddr, 0x80, 0x04|0x02|0x00)
+        smbus.sm_writeByte(i2cAddr, 0x80, 0x04|0x02|0x00)
     }
 
-    //% blockId="GROVE_I2C_LCD1602_CLEAR_DISPLAY" block="[LCD] clear display"
-    export function clearDisplay(): void {
-        smbus.writeByte(i2cAddr, 0x80, 0x01)
+    //% blockId="GROVE_I2C_LCD1602_CLEAR_DISPLAY" block="[LCD] Effacer"
+    export function LCD_clearDisplay(): void {
+        smbus.sm_writeByte(i2cAddr, 0x80, 0x01)
         basic.pause(2) // Attente > 1.53ms
     }
 
 
-    //% blockId="GROVE_I2C_LCD1602_DISPLAY_ONOFF" block="[LCD] display $on"
+    //% blockId="GROVE_I2C_LCD1602_DISPLAY_ONOFF" block="[LCD] Définir état $on"
     //% on.shadow=toggleOnOff
     //% on.defl=true
-    export function displayOnOff(on: boolean): void {
+    export function LCD_displayOnOff(on: boolean): void {
         if (on) regCTRL |= 0x04; else regCTRL &= ~0x04
-        smbus.writeByte(i2cAddr, 0x80, 0x08|regCTRL)
+        smbus.sm_writeByte(i2cAddr, 0x80, 0x08|regCTRL)
         basic.pause(1) // Attente > 39us
     }
 
-    //% blockId="GROVE_I2C_LCD1602_CURSOR_ONOFF" block="[LCD] cursor $on"
+    //% blockId="GROVE_I2C_LCD1602_CURSOR_ONOFF" block="[LCD] Curseur $on"
     //% on.shadow=toggleOnOff
     //% on.defl=true
-    export function cursorOnOff(on: boolean): void {
+    export function LCD_cursorOnOff(on: boolean): void {
         if (on) regCTRL |= 0x02; else regCTRL &= ~0x02
-        smbus.writeByte(i2cAddr, 0x80, 0x08|regCTRL)
+        smbus.sm_writeByte(i2cAddr, 0x80, 0x08|regCTRL)
         basic.pause(1) // Attente > 39us
     }
 
-    //% blockId="GROVE_I2C_LCD1602_CURSOR_BLINK_ONOFF" block="[LCD] cursor blink $on"
+    //% blockId="GROVE_I2C_LCD1602_CURSOR_BLINK_ONOFF" block="[LCD] Clignotement curseur $on"
     //% on.shadow=toggleOnOff
     //% on.defl=true
-    export function cursorBlinkOnOff(on: boolean): void {
+    export function LCD_cursorBlinkOnOff(on: boolean): void {
         if (on) regCTRL |= 0x01; else regCTRL &= ~0x01
-        smbus.writeByte(i2cAddr, 0x80, 0x08|regCTRL)
+        smbus.sm_writeByte(i2cAddr, 0x80, 0x08|regCTRL)
         basic.pause(1) // Attente > 39us
     }
 
@@ -135,12 +135,12 @@ namespace GROVE_I2C_LCD1602 {
          * @param x is LCD column position, [0 - 15], eg: 0
          * @param y is LCD row position, [0 - 1], eg: 0
          */
-    //% blockId="GROVE_I2C_LCD1620_SET_CURSOR_POSITION" block="[LCD] set cursor position at |x $x |y $y"
+    //% blockId="GROVE_I2C_LCD1620_SET_CURSOR_POSITION" block="[LCD] Définir position curseur à |x $x |y $y"
     //% x.min=0 x.max=15
     //% y.min=0 y.max=1
-    export function setCursorPosition(x: number, y: number): void {
+    export function LCD_setCursorPosition(x: number, y: number): void {
         if (y == 0) x |= 0x80; else x |= 0xc0
-        smbus.writeByte(i2cAddr, 0x80, x)
+        smbus.sm_writeByte(i2cAddr, 0x80, x)
         basic.pause(1) // Attente > 39us
     }
 
@@ -155,13 +155,13 @@ namespace GROVE_I2C_LCD1602 {
     //% s.defl="Hello"
     //% x.min=0 x.max=15
     //% y.min=0 y.max=1
-    export function showString(s: string, x: number, y: number): void {
+    export function LCD_showString(s: string, x: number, y: number): void {
         // Set cursor position
-        setCursorPosition(x,y)
+        LCD_setCursorPosition(x,y)
         
         // Display string characters
         for (let i = 0; i < s.length; i++) {
-            smbus.writeByte(i2cAddr, 0x40, s.charCodeAt(i))
+            smbus.sm_writeByte(i2cAddr, 0x40, s.charCodeAt(i))
         }
     }
 
@@ -175,9 +175,9 @@ namespace GROVE_I2C_LCD1602 {
     //% x.min=0 x.max=15
     //% y.min=0 y.max=1
     //% n.defl=0 
-    export function ShowNumber(n: number, x: number, y: number): void {
+    export function LCD_ShowNumber(n: number, x: number, y: number): void {
         let s = n.toString()
-        showString(s, x, y)
+        LCD_showString(s, x, y)
     }
 
 
